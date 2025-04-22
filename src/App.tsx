ae97084fca35ee87a9c7d7d1625a21b9a2b786db
tcrusel <crusel.thomas@gmail.com>
2025-04-22 17:27:18 +0200

    Begin US_03_discover_profiles

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,20 @@ function App() {
 	return (
 		<>
 			<header>
+				<img
+					className="logo"
+					src="src/images/logo.png"
+					alt="logo"
+					width="50"
+					height="50"
+				/>
+				<h1>RENDEZ-WOOF</h1>
 				<nav>
 					<Link to="/">Accueil</Link>
 					<Link to="/my_profile">Mon profil</Link>
 					<Link to="/discover_profiles">Découvrir des profils</Link>
 				</nav>
 			</header>
-			<h1>coucou</h1>
 			<h2>toto</h2>
 			<Outlet />
 		</>
