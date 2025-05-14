ce6b08e4003d81295dd2604deddc93a1151d7837
PIERRE_WIART <PWIART89@gmail.com>
2025-05-14 14:20:38 +0200

    modification

--- src/components/Match/myMatch.tsx
+++ src/components/Match/myMatch.tsx
@@ -1,45 +1,41 @@
 import { useEffect, useState } from "react";
 import CardProfile from "../CardProfile/CardProfile";
 import "../Recommandations/Recommandations.css";
+import useDogs from "../../API/API";
 import tidusProfile from "../../data/tidus.json";
 import type { DogType } from "../LikeContext/LikesContext";
 
 function myMatch() {
 	const [matchingDogs, setMatchingDogs] = useState<DogType[]>([]);
+	const dogs = useDogs();
 
 	useEffect(() => {
-		fetch(
-			"https://my-json-server.typicode.com/wildcodeschool-2025-03/JS-bordeaux-p2-api-rendez-woof/dogs",
-		)
-			.then((response) => response.json())
-			.then((data) => {
-				const dogs: DogType[] = data;
+		if (dogs.length === 0) return;
 
-				const traitsTidus = [
-					...tidusProfile.personality,
-					...tidusProfile.hobbies,
-					...tidusProfile.phobias,
-					...tidusProfile.favorite_foods,
-				];
+		const traitsTidus = [
+			...tidusProfile.personality,
+			...tidusProfile.hobbies,
+			...tidusProfile.phobias,
+			...tidusProfile.favorite_foods,
+		];
 
-				const result = dogs.filter((dog) => {
-					const dogTraits = [
-						...dog.personality,
-						...dog.hobbies,
-						...dog.phobias,
-						...dog.favorite_foods,
-					];
+		const result = dogs.filter((dog) => {
+			const dogTraits = [
+				...dog.personality,
+				...dog.hobbies,
+				...dog.phobias,
+				...dog.favorite_foods,
+			];
 
-					const commonTraits = dogTraits.filter((trait) =>
-						traitsTidus.includes(trait),
-					);
+			const commonTraits = dogTraits.filter((trait) =>
+				traitsTidus.includes(trait),
+			);
 
-					return commonTraits.length >= 8;
-				});
+			return commonTraits.length >= 8;
+		});
 
-				setMatchingDogs(result);
-			});
-	}, []);
+		setMatchingDogs(result);
+	}, [dogs]);
 
 	const removeDogFromList = (id: number) => {
 		setMatchingDogs((prevDogs) => prevDogs.filter((dog) => dog.id !== id));
--- src/components/Recommandations/Recommandations.tsx
+++ src/components/Recommandations/Recommandations.tsx
@@ -1,45 +1,41 @@
 import { useEffect, useState } from "react";
 import CardProfile from "../CardProfile/CardProfile";
 import "../Recommandations/Recommandations.css";
+import useDogs from "../../API/API";
 import tidusProfile from "../../data/tidus.json";
 import type { DogType } from "../LikeContext/LikesContext";
 
 function Recommandations() {
 	const [matchingDogs, setMatchingDogs] = useState<DogType[]>([]);
+	const fetchDogs = useDogs();
 
 	useEffect(() => {
-		fetch(
-			"https://my-json-server.typicode.com/wildcodeschool-2025-03/JS-bordeaux-p2-api-rendez-woof/dogs",
-		)
-			.then((response) => response.json())
-			.then((data) => {
-				const dogs: DogType[] = data;
+		if (fetchDogs.length === 0) return;
 
-				const traitsTidus = [
-					...tidusProfile.personality,
-					...tidusProfile.hobbies,
-					...tidusProfile.phobias,
-					...tidusProfile.favorite_foods,
-				];
+		const traitsTidus = [
+			...tidusProfile.personality,
+			...tidusProfile.hobbies,
+			...tidusProfile.phobias,
+			...tidusProfile.favorite_foods,
+		];
 
-				const result = dogs.filter((dog) => {
-					const dogTraits = [
-						...dog.personality,
-						...dog.hobbies,
-						...dog.phobias,
-						...dog.favorite_foods,
-					];
+		const result = fetchDogs.filter((dog) => {
+			const dogTraits = [
+				...dog.personality,
+				...dog.hobbies,
+				...dog.phobias,
+				...dog.favorite_foods,
+			];
 
-					const commonTraits = dogTraits.filter((trait) =>
-						traitsTidus.includes(trait),
-					);
+			const commonTraits = dogTraits.filter((trait) =>
+				traitsTidus.includes(trait),
+			);
 
-					return commonTraits.length >= 5;
-				});
+			return commonTraits.length >= 5;
+		});
 
-				setMatchingDogs(result);
-			});
-	}, []);
+		setMatchingDogs(result);
+	}, [fetchDogs]);
 
 	const removeDogFromList = (id: number) => {
 		setMatchingDogs((prevDogs) => prevDogs.filter((dog) => dog.id !== id));
